8a5c6af3c0603e09f5209013eab6e8360b48bd25
Adam Ghadban <ghadbanadam@cs.technion.ac.il>
2023-06-17 20:01:30 +0300

    part3 done and part1 done

--- hw3/answers.py
+++ hw3/answers.py
@@ -35,22 +35,31 @@ def part1_generation_params():
 
 part1_q1 = r"""
 **Your answer:**
-
+we split the text into sequences for several reasons:
+1. parallilism of our training thus faster training.
+2. back propagation on large text might lead to vanishing/exploding gradients, thus training will be impossible.
 """
 
 part1_q2 = r"""
 **Your answer:**
-
+our model can remember with no limit to the "past", due to the fact that the update and reset gate decide how much
+the next state is affected by the previous state.
 """
 
 part1_q3 = r"""
 **Your answer:**
-
+because we want to retain the semantics and meaning between sequences and have a 
+more logical and plausable output of the model.
 """
 
 part1_q4 = r"""
 **Your answer:**
+1) we lower the temperature in order to shift the distribution to a more heterogenous distribution, thus making the generated sequences more 
+related to what is learnt.
 
+2) when the temp is very high, the distribution will be more uniform, thus sampling from the distribution will yeild random input.
+
+3) when the temp is very low, hot-softmax will behave like argmax, thus the letter with the highest distribution will be sampled. 
 
 """
 # ==============
@@ -108,13 +117,13 @@ PART3_CUSTOM_DATA_URL = None
 
 def part3_transformer_encoder_hyperparams():
     hypers = dict(
-        embed_dim = 0, 
-        num_heads = 0,
-        num_layers = 0,
-        hidden_dim = 0,
-        window_size = 0,
-        droupout = 0.0,
-        lr=0.0,
+        embed_dim = 128, 
+        num_heads = 4,
+        num_layers = 3,
+        hidden_dim = 192,
+        window_size = 128,
+        droupout = 0.20,
+        lr=0.0004,
     )
 
     # TODO: Tweak the hyperparameters to train the transformer encoder.
@@ -128,13 +137,13 @@ def part3_transformer_encoder_hyperparams():
 
 part3_q1 = r"""
 **Your answer:**
-
+stacking multiple layers in a CNN resuls in that each layer processes an input that was processed by the previous layer, thus each layer increases the total receptive field of the input.
+in a sliding window attention, each layer processes an input from a larger context that was captured by the previous layer, thus increasing the receptive field
 """
 
 part3_q2 = r"""
 **Your answer:**
-
-
+a trivial answer is to make the window bigger, thus having a more global context than a small window (check with abugosh)
 """
 
 
--- hw3/transformer.py
+++ hw3/transformer.py
@@ -1,10 +1,116 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import math
 
 
+def pad_tensors_to_window_size(q, k , v, half_window_size, padding_mask, paddin_value=0):
+    seq_size = q.shape[-2]
+    window = 2 * half_window_size
+    padding_len = (window - seq_size % window) % window
+    padding_l, padding_r = (padding_len//2, padding_len//2) if window > 2 else (0, 1)
+    padding_dim = (0, 0, padding_l, padding_r)
+    q = F.pad(q, padding_dim, value=paddin_value)
+    k = F.pad(k, padding_dim, value=paddin_value)
+    v = F.pad(v, padding_dim, value=paddin_value)
+    if padding_mask is not None:
+        padding_mask = F.pad(padding_mask, padding_dim[2:], value=0)
+    return q, k, v, padding_mask
+
+def _get_wrong_locations_mask(window, device):
+    diags = []
+    for j in range(-window, 1):
+        diag_mask = torch.zeros(window, device='cpu', dtype=torch.uint8)
+        diag_mask[:-j] = 1
+        diags.append(diag_mask)
+    mask = torch.stack(diags, dim=-1)
+    mask = mask[None, :, None, :]
+    mask_end = mask.flip(dims=(1, 3)).bool().to(device)
+    return mask.bool().to(device), mask_end
+
+def mask_wrong_elements(x, half_window_size):
+    # get masks
+    mask_begin, mask_end = _get_wrong_locations_mask(half_window_size, x.device)
+    #apply begin mask
+    seq_size = x.size(1)
+    input_begin = x[:, :half_window_size, :, :half_window_size+1]
+    mask_begin = mask_begin[:, :seq_size].expand_as(input_begin)
+    input_begin.masked_fill_(mask_begin, -9e15)
+    # apply end mask
+    input_end = x[:, -half_window_size:, :, -(half_window_size+1):]
+    mask_end = mask_end[:, -seq_size:].expand_as(input_end)
+    input_end.masked_fill_(mask_end, -9e15)
 
 
+def get_main_diagonals_indices(batches, seq_size, window):
+    diag = torch.arange(-window, window + 1)
+    row = torch.arange(0, seq_size * seq_size, seq_size + 1)
+    col = row.reshape(1, -1, 1) + diag
+    col = col.repeat(batches, 1, 1)
+    return col.flatten(1)[:, window:-window]
+
+def populate_diags(x):
+    batches, seq_size, window = x.size()
+    window = (window - 1)//2
+    x= x.flatten(1)[:, window:-window].float()
+    res = torch.zeros(batches, seq_size, seq_size, device=x.device).flatten(1)
+    idx = get_main_diagonals_indices(batches, seq_size, window).to(x.device)
+    res= res.scatter_(1, idx, x).view(batches, seq_size, seq_size)
+    return res
+
+
+def sliding_chunks_matmul_qk(q, k, half_window_size, padding_value):
+    batches, number_of_heads, seq_size, head_dim = q.size()
+    q = q.reshape(batches * number_of_heads, seq_size, head_dim)
+    k = k.reshape(batches * number_of_heads, seq_size, head_dim)
+
+    k_chunk = k.unfold(-2, 2*half_window_size, half_window_size).transpose(-1, -2)
+    q_chunk = q.unfold(-2, 2*half_window_size, half_window_size).transpose(-1, -2)
+    chunk_attention = torch.matmul(q_chunk, k_chunk.transpose(-2, -1))
+    # convert diags into cols
+    chunk_attention = nn.functional.pad(chunk_attention, (0, 0, 0, 1), value=padding_value)
+    diag_chunk_attention = chunk_attention.view(*chunk_attention.size()[:-2], chunk_attention.size(-1), chunk_attention.size(-2))
+
+    number_chunks = seq_size // half_window_size - 1
+    # this will be the overall attention
+    diag_attention = torch.full((batches * number_of_heads, number_chunks + 1, half_window_size, half_window_size * 2 + 1), fill_value=1, device=chunk_attention.device)*(-9e15)
+
+    # copy upper tri and main diag
+    diag_attention[:, :-1, :, half_window_size:] = diag_chunk_attention[:, :, :half_window_size, :(half_window_size + 1)]
+    diag_attention[:, -1, :, half_window_size:] = diag_chunk_attention[:, -1, half_window_size:, :(half_window_size + 1)]
+   # copy lower tri
+    diag_attention[:, 1:, :, :half_window_size] = diag_chunk_attention[:, :, - (half_window_size + 1):-1, (half_window_size + 1):]
+    diag_attention[:, 0, 1:half_window_size, 1:half_window_size] = diag_chunk_attention[:, 0, :(half_window_size - 1), ((half_window_size > 1) - half_window_size):]
+    # restore to original dims
+    diag_attention = diag_attention.reshape(batches, number_of_heads, seq_size, 2 * half_window_size + 1).transpose(2, 1)
+    mask_wrong_elements(diag_attention, half_window_size)
+    diag_attention = diag_attention.transpose(1,2).reshape(batches * number_of_heads, seq_size, 2 * half_window_size + 1)
+    return diag_attention
+
+
+
+def sliding_chunks_matmul_pv(probabilities, values, half_window_size):
+
+    batches, seq_size, number_of_heads, head_dim = values.size()
+    chunks_count = seq_size // half_window_size - 1
+    chunk_prob = probabilities.transpose(1,2).reshape(batches * number_of_heads, seq_size // half_window_size, half_window_size, 2 * half_window_size + 1)
+
+    values = values.transpose(1, 2).reshape(batches * number_of_heads, seq_size, head_dim)
+    values = F.pad(values, (0, 0, half_window_size, half_window_size), value=-1)
+
+    chunk_size = (batches * number_of_heads, chunks_count + 1, 3 * half_window_size, head_dim)
+    chunk_stride = values.stride()
+    chunk_stride = chunk_stride[0], half_window_size * chunk_stride[1], chunk_stride[1], chunk_stride[2]
+    chunk_v = values.as_strided(size=chunk_size, stride=chunk_stride)
+    B, C, M, L = chunk_prob.size()
+    chunk_prob = F.pad(chunk_prob, (0, M + 1), value=0)
+    chunk_prob = chunk_prob.reshape(B, C, -1)
+    chunk_prob = chunk_prob[:, :, :-M]
+    chunk_prob = chunk_prob.reshape(B, C, M, M + L)
+    skewed_prob = chunk_prob[:, :, :, :-1]
+    context = torch.matmul(skewed_prob, chunk_v)
+    return context.reshape(batches, number_of_heads, seq_size, head_dim)
+    
 
 def sliding_window_attention(q, k, v, window_size, padding_mask=None):
     '''
@@ -19,11 +125,11 @@ def sliding_window_attention(q, k, v, window_size, padding_mask=None):
     :return attention - the attention weights. #[Batch, SeqLen, SeqLen] or [Batch, num_heads, SeqLen, SeqLen]
     '''
     assert window_size%2 == 0, "window size must be an even number"
-    seq_len = q.shape[-2]
-    embed_dim = q.shape[-1]
+    seq_size = q.shape[-2]
     batch_size = q.shape[0] 
-
+    embed_dim = q.shape[-1]
     values, attention = None, None
+
 
     # TODO:
     #  Compute the sliding window attention.
@@ -33,13 +139,46 @@ def sliding_window_attention(q, k, v, window_size, padding_mask=None):
     #    (both for tokens that aren't in the window, and for tokens that correspond to padding according to the 'padding mask').
     # Aside from these two rules, you are free to implement the function as you wish. 
     # ====== YOUR CODE: ======
-    raise NotImplementedError()
-    # ========================
+    # Compute the sliding window attention
+     # Compute left and right padding based on the window size
 
+    half_window_size = window_size //2 
 
+    single_head = False
+    if len(q.shape) == 3:
+        single_head = True
+        q = q.unsqueeze(1)
+    if len(k.shape) == 3:
+        k = k.unsqueeze(1)
+    if len(v.shape) == 3:
+        v = v.unsqueeze(1)
+    number_of_heads = q.shape[1]
+    q, k, v, padding_mask = pad_tensors_to_window_size(q, k, v, half_window_size, padding_mask)
+    new_seq_len = q.shape[-2]
+    attention_weights = sliding_chunks_matmul_qk(q, k, half_window_size, padding_value=-9e15).view(batch_size, number_of_heads, new_seq_len, 2 * half_window_size + 1)
+
+    if padding_mask is not None:
+        padding_mask = torch.logical_not(padding_mask.unsqueeze(dim=1).unsqueeze(dim=-1))
+        padding_mask = padding_mask.type_as(q).masked_fill(padding_mask, -9e15)
+        ones = torch.ones_like(padding_mask)
+        d_mask = sliding_chunks_matmul_qk(ones, padding_mask, half_window_size, padding_value=-9e15).view(batch_size, 1, new_seq_len, 2 * half_window_size + 1)  
+        attention_weights += d_mask 
+    attention =  torch.nn.functional.softmax(attention_weights / math.sqrt(embed_dim), dim=-1).transpose(1,2)
+
+    values = sliding_chunks_matmul_pv(attention, v.transpose(1,2), half_window_size) #[batch_size,num_heads,seq_len, embed_dim]
+    attention = attention.transpose(2,1)
+    attention = attention.reshape(batch_size * number_of_heads, new_seq_len, 2 * half_window_size + 1)
+    attention = populate_diags(attention)
+    attention = attention.reshape(batch_size, number_of_heads, new_seq_len, new_seq_len)
+    if new_seq_len != seq_size:
+        padding_len = new_seq_len - seq_size
+        padding_left, padding_right = (padding_len//2, padding_len//2) if padding_len > 1 else (0, 1)
+        attention = attention[:, :, padding_left:-padding_right, padding_left:-padding_right]
+        values = values[:, :, padding_left:-padding_right, :]
+    if single_head:
+        values = values.squeeze(1)
+        attention = attention.squeeze(1)        
     return values, attention
-
-
 
 class MultiHeadAttention(nn.Module):
     
@@ -80,7 +219,7 @@ class MultiHeadAttention(nn.Module):
         # TODO:
         # call the sliding window attention function you implemented
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        values, attention = sliding_window_attention(q, k, v, self.window_size, padding_mask)
         # ========================
 
         values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
@@ -162,7 +301,12 @@ class EncoderLayer(nn.Module):
         #   3) Apply a feed-forward layer to the output of step 2, and then apply dropout again.
         #   4) Add a second residual connection and normalize again.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        weighted_values = self.self_attn(x, padding_mask)
+        dropout = self.dropout(weighted_values)
+        normalized_weighted_vals = self.norm1(x + dropout)
+
+        dropout = self.dropout(self.feed_forward(normalized_weighted_vals))
+        x = self.norm2(dropout + normalized_weighted_vals)
         # ========================
         
         return x
@@ -212,7 +356,14 @@ class Encoder(nn.Module):
         #  5) Apply the classification MLP to the output vector corresponding to the special token [CLS] 
         #     (always the first token) to receive the logits.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        embedding = self.encoder_embedding.forward(sentence)
+        positional_encoding = self.positional_encoding.forward(embedding)
+        encoded_out = self.dropout(positional_encoding)
+
+        for layer in self.encoder_layers:
+            encoded_out = layer(encoded_out, padding_mask)
+        
+        output = self.classification_mlp(encoded_out[:, 0, :]).squeeze(-1)
         
         # ========================
         
@@ -230,4 +381,4 @@ class Encoder(nn.Module):
         return preds
 
     
-    
+
